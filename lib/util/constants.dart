e08fd77453cacce7b4b7dff1508e9adbedf91afe
ZanderCowboy <zanderkotze99@gmail.com>
2023-03-25 03:40:57 +0200

    Added new constants for Discord

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -6,15 +6,20 @@ const String githubURL = 'https://github.com/ZanderCowboy';
 const String personalWebsiteURL = 'https://zanderkotze.co.za/website';
 const String youtubeURL = 'https://youtube.com/@zanderkotze';
 const String linkedInURL = 'https://linkedin.com/in/zander-kotze-5ba18abb/';
+const String discordURL = 'https://discordapp.com/users/641236287671631883';
+// Steam?
+// TikTok?
+// WhatsApp?
 
 
 const Color twitterColor = Color(0xFF1DA1F2);
 const Color instagramColor = Colors.transparent;
 const Color githubColor = Color(0xFF171515);
 const Color personalWebsiteColor = Colors.grey;
-const Color youtubeColor = Color(0xFFFF0000);
-// const Color youtubeColor = Color(0xFFCD201F);
+// const Color youtubeColor = Color(0xFFFF0000);
+const Color youtubeColor = Color(0xFFCD201F);
 const Color linkedInColor = Color(0xFF0077B5);
+const Color discordColor = Color(0xFF5865F2);
 
 const int _SIZE = 384;
 // const String _TWITTER = 'twitter';
@@ -30,3 +35,5 @@ const String githubImage = "images/github/icons8-github-$_SIZE.svg";
 const String personalWebsiteImage = 'images/instagram/icons8-instagram-$_SIZE.svg';
 const String youtubeImage = 'images/youtube/icons8-youtube-$_SIZE.svg';
 const String linkedInImage = 'images/linkedin/icons8-linkedin-$_SIZE.svg';
+// const String discordImage = 'images/discord/discord-mark-white.svg';
+const String discordImage = 'images/discord/icons8-discord-new-$_SIZE.svg';
