d973f4d776625002446f7cb05b01b897d5e80754
ZanderCowboy <zanderkotze99@gmail.com>
2023-03-26 17:07:06 +0200

    initial version, release

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,3 @@
 FROM nginx:alpine
 
 COPY build/web /usr/share/nginx/html
-
-RUN mv /usr/share/nginx/html/assets/assets/ /usr/share/nginx/html/assets
-
-RUN ls /usr/share/nginx/html/assets
--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -21,7 +21,7 @@ const Color youtubeColor = Color(0xFFCD201F);
 const Color linkedInColor = Color(0xFF0077B5);
 const Color discordColor = Color(0xFF5865F2);
 
-const int _SIZE = 384;
+// const int _SIZE = 384;
 // const String _TWITTER = 'twitter';
 // const String instagram = 'instagram';
 // const String github = 'github';
@@ -29,11 +29,11 @@ const int _SIZE = 384;
 // const String youtube = 'youtube';
 // const String linkedIn = 'linkedin';
 
-const String twitterImage = 'images/twitter/icons8-twitter-$_SIZE.svg';
-const String instagramImage = 'images/instagram/icons8-instagram-$_SIZE.svg';
-const String githubImage = "images/github/icons8-github-$_SIZE.svg";
-const String personalWebsiteImage = 'images/instagram/icons8-instagram-$_SIZE.svg';
-const String youtubeImage = 'images/youtube/icons8-youtube-$_SIZE.svg';
-const String linkedInImage = 'images/linkedin/icons8-linkedin-$_SIZE.svg';
+const String twitterImage = 'images/twitter/icons8-twitter-384.svg';
+const String instagramImage = 'images/instagram/icons8-instagram-384.svg';
+const String githubImage = "images/github/icons8-github-384.svg";
+const String personalWebsiteImage = 'images/instagram/icons8-instagram-384.svg';
+const String youtubeImage = 'images/youtube/icons8-youtube-384.svg';
+const String linkedInImage = 'images/linkedin/icons8-linkedin-384.svg';
 // const String discordImage = 'images/discord/discord-mark-white.svg';
-const String discordImage = 'images/discord/icons8-discord-new-$_SIZE.svg';
+const String discordImage = 'images/discord/icons8-discord-new-384.svg';
