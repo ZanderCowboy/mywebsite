edff1586d15687ff76dd2af8e664b1a91003267b
ZanderCowboy <zanderkotze99@gmail.com>
2023-03-27 15:29:42 +0200

    Cleared some things, reformat

--- lib/components/copyright/copyright.dart
+++ lib/components/copyright/copyright.dart
@@ -6,16 +6,17 @@ class Copyright extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String date = DateTime.now().year.toString();
+    // const IconData copyright = IconData(0xe198, fontFamily: 'MaterialIcons');
 
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Text(
-          date + ' ',
-          style: TextStyle(
+          '$date ',
+          style: const TextStyle(
             color: Colors.white,
             fontSize: 12,
-            ),
+          ),
         ),
         const Icon(
           Icons.copyright,
