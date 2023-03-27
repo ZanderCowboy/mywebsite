decccbf928eb94f7b8801d566a841de3f6f22dff
ZanderCowboy <zanderkotze99@gmail.com>
2023-03-27 15:33:20 +0200

    Added a background image; added structuring; changed button; added text

--- lib/views/firstpage.dart
+++ lib/views/firstpage.dart
@@ -1,28 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:mywebsite/route/route.dart' as route;
 
-
 class FirstPage extends StatelessWidget {
   const FirstPage({Key? key}) : super(key: key);
 
-  // const FirstRoute({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('First Route'),
-      ),
+    // double PAGE_HEIGHT = MediaQuery.of(context).size.height;
+    // double PAGE_WIDTH = MediaQuery.of(context).size.width;
 
-      body: Center(
-        child: ElevatedButton(
-          child:
-            const Text('Open route', style: TextStyle(color: Colors.white),
+    return Scaffold(
+      // resizeToAvoidBottomInset: false,
+      body: Stack(
+        // fit: StackFit.expand,
+        children: [
+          Positioned.fill(
+            child: Image.network(
+              "https://images.pexels.com/photos/2049422/pexels-photo-2"
+              "049422.jpeg?cs=srgb&dl=pexels-pok-rie-2049422.jpg&fm=jpg&_gl=1*dm5gf8*_ga*M"
+              "TAyNDYxOTY2Mi4xNjc5NzA0NDQx*_ga_8JE65Q40S6*MTY3OTkxMTAxMy43LjAuMTY3OTkxMTAx"
+              "My4wLjAuMA..",
+              // width: PAGE_WIDTH,
+              fit: BoxFit.cover,
             ),
-            onPressed: () => Navigator.pushNamed(context, route.secondPage),
-        ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width,
+                alignment: Alignment.center,
+                child: const Text(
+                  'Coming Soon',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 100,
+                    fontFamily: 'Anton',
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                child: const Text(
+                  'Return to Home Page',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () => Navigator.pushNamed(context, route.homePage),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
 }
-
