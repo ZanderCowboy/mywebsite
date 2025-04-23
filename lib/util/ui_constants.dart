19db289dbebed906f141e0b882ef25f83819e33d
ZanderCowboy <zanderkotze99@gmail.com>
2025-04-23 18:30:12 +0200

    use gap package and asset.gen

--- lib/util/ui_constants.dart
+++ lib/util/ui_constants.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 const double buttonSpaceHeight = 10;
 const double buttonSpaceWidth = 320;
 
-// Spacing Contants
+// Spacing Constants
 
 const sizedBox = SizedBox(
   width: buttonSpaceWidth,
   height: buttonSpaceHeight,
 );
 
-const width8 = SizedBox(width: 8);
-const height4 = SizedBox(height: 4);
-const height8 = SizedBox(height: 8);
-const height12 = SizedBox(height: 12);
-const height18 = SizedBox(height: 18);
-const height24 = SizedBox(height: 24);
-const height40 = SizedBox(height: 40);
+const gap4 = Gap(4);
+const gap8 = Gap(8);
+const gap12 = Gap(12);
+const gap16 = Gap(16);
+const gap18 = Gap(18);
+const gap20 = Gap(20);
+const gap24 = Gap(24);
+const gap40 = Gap(40);
 
 // Padding Constants
 const allPadding4 = EdgeInsets.all(4);
@@ -27,6 +29,7 @@ const allPadding32 = EdgeInsets.all(32);
 const allPadding40 = EdgeInsets.all(40);
 const vertical20horizontal10 =
     EdgeInsets.symmetric(vertical: 20, horizontal: 10);
+const vertical12 = EdgeInsets.symmetric(vertical: 12);
 
 // Border Constants
 final borderRadius5 = BorderRadius.circular(5);
--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:mywebsite/components/copyright.dart';
 import 'package:mywebsite/components/link_button.dart';
+import 'package:mywebsite/gen/assets.gen.dart';
 import 'package:mywebsite/util/constants.dart';
 import 'package:mywebsite/util/ui_constants.dart';
 import 'package:transparent_image/transparent_image.dart';
